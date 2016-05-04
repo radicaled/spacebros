7b7522b9b973dd2edceb6bb836128d3b736a739e
Arron Washington <arron@theradicaledwards.com>
2016-05-04 04:56:07 -0400

    Initial import of whack wizardry

---
+++ desktop/src/spacebros/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package spacebros.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import spacebros.game.SpacebrosGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SpacebrosGame(), config);
+	}
+}
