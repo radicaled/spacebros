8ec1ce04c74e394823d6cad97311cd997506a401
Arron Washington <arron@theradicaledwards.com>
2016-12-15 22:44:44 -0500

    Add an inventory component

---
+++ server/src/spacebros/server/game/components/InventoryComponent.kt
@@ -0,0 +1,13 @@
+package spacebros.server.game.components
+
+import com.artemis.Component
+import spacebros.server.game.components.annotations.ClientValue
+import spacebros.server.game.components.annotations.ComponentName
+import java.util.*
+
+@ComponentName("inventory")
+class InventoryComponent : Component() {
+    // TODO: only sync this to clients that need to know about inventory contents
+    @ClientValue("contents")
+    val contents: ArrayList<Int> = arrayListOf()
+}
